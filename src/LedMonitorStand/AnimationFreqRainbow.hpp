7175c3ee1942a7397e920819c2f5a576c5fc58bd
tom0334 <tfriederich@live.nl>
2018-11-10 22:48:22 +0100

    Made the brightness of the white center leds adaptive

--- src/LedMonitorStand/AnimationFreqRainbow.hpp
+++ src/LedMonitorStand/AnimationFreqRainbow.hpp
@@ -5,6 +5,8 @@
 #ifndef ANIMATION_FREQ_RAINBOW
 #define ANIMATION_FREQ_RAINBOW
 
+#define CENTER_LED_BRIGHTNESS_HISTORY 20
+
 class AnimationFreqRainbow : public MusicAnimation {
 
   public:
@@ -14,16 +16,12 @@ class AnimationFreqRainbow : public MusicAnimation {
       if (adjust == GREEN_MODE) {
         beatFactor = 1 + potInput / 64;
       }
-
-      //show the white leds in the center
-      for (int i = RIGHT_EDGE + 1; i < LEFT_EDGE; i++) {
-        leds[i] =  CRGB(255, 255, 255);
-      }
       CRGB shiftColor = getColor(measurer);
 
       leds[RIGHT_EDGE] =  shiftColor;
       leds[LEFT_EDGE] =  shiftColor;
       shift();
+      updateCenterLeds();
     }
 
   private:
@@ -37,7 +35,6 @@ class AnimationFreqRainbow : public MusicAnimation {
 
       if (red < green  && red < blue) {
         red = 0;
-        //todo check if this is nice.
         green /= beatFactor;
         blue /= beatFactor;
       }
@@ -49,6 +46,29 @@ class AnimationFreqRainbow : public MusicAnimation {
         blue = 0;
       }
       return CRGB(min(255, red), min(255 / beatFactor, green), min(255 / beatFactor, blue));
+    }
+
+    //aproximation of the brightness of a led, not very accurate but it works well enough for these purposes.
+    //It's an aproximation of this: Y = 0.2126 R + 0.7152 G + 0.0722 B
+    uint8_t brightness(CRGB & c) {
+       return floor(0.299f * (float) c.r + 0.587 * (float) c.g  + 0.114 * (float) c.b);
+    }
+
+    //this updates the brightness of the center leds.
+    //it is dependent on the brightness of the last few leds, determined by the 
+    // CENTER_LED_BRIGHTNESS_HISTORY
+    void updateCenterLeds() {
+      int sum = 0;
+      for (int i = LEFT_EDGE; i < LEFT_EDGE + CENTER_LED_BRIGHTNESS_HISTORY; i++) {
+        sum+= brightness( leds[i]);
+      }
+      uint8_t avg = sum / CENTER_LED_BRIGHTNESS_HISTORY;
+
+      //min brightness is 128
+      uint8_t result = min( avg/2 + 128 ,255);
+      for (int i = RIGHT_EDGE + 1; i < LEFT_EDGE; i++) {
+        leds[i] = CHSV(0, 0, result);
+      }
     }
 
     //Shifts the center color to the sides.
