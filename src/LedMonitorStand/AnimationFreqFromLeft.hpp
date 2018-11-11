4dc4c20da6a2c1adcdd146b935dd284c0aa485d8
tom0334 <tfriederich@live.nl>
2018-11-11 13:25:11 +0100

    Rename AnimationFreqRainbow to AnimationFreq and add comments

--- src/LedMonitorStand/AnimationFreqFromLeft.hpp
+++ src/LedMonitorStand/AnimationFreqFromLeft.hpp
@@ -1,15 +1,18 @@
 #include "Constants.hpp"
 #include "Measurer.hpp"
 #include "Animation.hpp"
-#include "AnimationFreqRainbow.hpp"
+#include "AnimationFreq.hpp"
 
 #ifndef ANIMATION_FREQ_FROM_LEFT
 #define ANIMATION_FREQ_FROM_LEFT
 
-class AnimationFreqFromLeft: public AnimationFreqRainbow {
+/**
+   A AnimationFreq that moves from the left side to the right side. 
+*/
+class AnimationFreqFromLeft: public AnimationFreq {
 
   public:
-    void setColor(CRGB newColor) {
+    void setColors(CRGB newColor) {
       leds[FRONT_LEFT] =  newColor;
       shiftRight(FRONT_LEFT, FRONT_RIGHT);
     }
--- src/LedMonitorStand/AnimationFreqMidleOut.hpp
+++ src/LedMonitorStand/AnimationFreqMidleOut.hpp
@@ -1,33 +1,46 @@
 #include "Constants.hpp"
 #include "Measurer.hpp"
 #include "Animation.hpp"
-#include "AnimationFreqRainbow.hpp"
+#include "AnimationFreq.hpp"
 
 #ifndef ANIMATION_FREQ_MIDLE_OUT
 #define ANIMATION_FREQ_MIDLE_OUT
 
-class AnimationFreqMidleOut: public AnimationFreqRainbow {
+#define CENTER_LED_BRIGHTNESS_HISTORY 20
+#define CENTER_LED_MIN_BRIGHTNESS 128
+
+/**
+ * A freq animation that moves from the midle out, and shows white leds in the center. 
+ * The brightness of the white leds is dependent on the prevous inputs.
+ */
+class AnimationFreqMidleOut: public AnimationFreq {
 
   public:
+    //OVERRIDE
     void setColors(CRGB newColor) {
       leds[RIGHT_EDGE] =  newColor;
       leds[LEFT_EDGE] =  newColor;
-      shiftLeft(LEFT_EDGE,LEFT_END);
-      shiftRight(RIGHT_EDGE,RIGHT_END);
+      shiftLeft(LEFT_EDGE, LEFT_END);
+      shiftRight(RIGHT_EDGE, RIGHT_END);
       updateCenterLeds();
     }
 
 
   private:
-    //aproximation of the brightness of a led, not very accurate but it works well enough for these purposes.
-    //It's an aproximation of this: Y = 0.2126 R + 0.7152 G + 0.0722 B
+    /**
+      aproximation of the brightness of a a led, accurate enough for these purposes.
+      see the wikipedia page on relative luminance for more info
+    */
     uint8_t brightness(CRGB & c) {
       return floor(0.299f * (float) c.r + 0.587 * (float) c.g  + 0.114 * (float) c.b);
     }
 
-    //this updates the brightness of the center leds.
-    //it is dependent on the brightness of the last few leds, determined by the
-    // CENTER_LED_BRIGHTNESS_HISTORY
+    /**
+      this updates the brightness of the center leds, by computing the brightness of leds further from the center,
+      and then calucalting the average brightness of those, plus some constant factor.
+      
+      The amount of leds to consider is determined by the CENTER_LED_BRIGHTNESS_HISTORY
+    */
     void updateCenterLeds() {
       int sum = 0;
       for (int i = LEFT_EDGE; i < LEFT_EDGE + CENTER_LED_BRIGHTNESS_HISTORY; i++) {
@@ -36,7 +49,7 @@ class AnimationFreqMidleOut: public AnimationFreqRainbow {
       uint8_t avg = sum / CENTER_LED_BRIGHTNESS_HISTORY;
 
       //min brightness is 128
-      uint8_t result = min( avg / 2 + 128 , 255);
+      uint8_t result = min( avg / 2 + CENTER_LED_MIN_BRIGHTNESS , 255);
       for (int i = RIGHT_EDGE + 1; i < LEFT_EDGE; i++) {
         leds[i] = CHSV(0, 0, result);
       }
